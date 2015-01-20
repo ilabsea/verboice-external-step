f38b643aa08dbeac03e29e066bd8d65cd6d17d38
kakada <mac@MacBookPro.local>
2015-01-20 17:37:06 +0700

    manage user for admin account

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+	def controller_name
+		names = params[:controller].split('/')
+		return '' if names.last == 'home'
+		return 'User List' if names.last == 'accounts'
+		return 'Setting' if names.last == 'registrations'
+		names.last
+	end
+
+	def action_name
+		return '' if params[:action] == 'index'
+		return 'new' if params[:action] == 'create'
+		params[:action]
+	end
+
 end
