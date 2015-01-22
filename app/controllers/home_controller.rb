447af2bd1022efda66fbaf631a32b1a7639d9de1
kakada <mac@MacBookPro.local>
2015-01-22 14:29:34 +0700

    set home page to step dashboard for signed in user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    unless user_signed_in?
-      redirect_to new_user_session_path
+    if user_signed_in?
+      redirect_to steps_path
+    else
+    	redirect_to new_user_session_path
     end
   end
 
