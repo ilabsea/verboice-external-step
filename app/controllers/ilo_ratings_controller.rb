ce39e7b918f693ddaee8aa970ce9c11118eb5843
kakada <mac@MacBookPro.local>
2015-02-02 14:55:13 +0700

    fix missing variable on sync form

--- app/controllers/ilo_ratings_controller.rb
+++ app/controllers/ilo_ratings_controller.rb
@@ -1,66 +1,67 @@
 class IloRatingsController < ApplicationController
-	def new
-		@step = Step.find_by_name Step::ILO_RATING
-		@rating = IloRating.new step_id: @step.id
-	end
+  def new
+    @step = Step.find_by_name Step::ILO_RATING
+    @rating = IloRating.new step_id: @step.id
+  end
 
-	def edit
-		@rating = IloRating.find(params[:id])
-	end
+  def edit
+    @rating = IloRating.find(params[:id])
+  end
 
-	def create
-		@rating = IloRating.new(protected_advance_params)
-		if @rating.save
-			redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been created'
-		else
-			flash.now[:alert] = 'Fail to create'
-			render :new, ilo_rating: @rating
-		end
-	end
+  def create
+    @rating = IloRating.new(protected_advance_params)
+    if @rating.save
+      redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been created'
+    else
+      flash.now[:alert] = 'Fail to create'
+      render :new, ilo_rating: @rating
+    end
+  end
 
-	def update
-		@rating = IloRating.find(params[:id])
-		if @rating.update_attributes(protected_basic_params)
-			redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been updated'
-		else
-			flash.now[:alert] = 'Fail to update'
-			render :edit
-		end
-	end
+  def update
+    @rating = IloRating.find(params[:id])
+    if @rating.update_attributes(protected_basic_params)
+      redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been updated'
+    else
+      flash.now[:alert] = 'Fail to update'
+      render :edit
+    end
+  end
 
-	def destroy
-		@rating = IloRating.find(params[:id])
-		if @rating.destroy
-			redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been deleted'
-		end
-	end
+  def destroy
+    @rating = IloRating.find(params[:id])
+    if @rating.destroy
+      redirect_to edit_step_path(@rating.step.name), notice: 'Rating has been deleted'
+    end
+  end
 
-	def show_sync
-		@rating = IloRating.find(params[:id])
-		@variables = Service::ProjectVariable.collection project_id: @rating.step.project_id
-	end
+  def show_sync
+    @rating = IloRating.find(params[:id])
+    @variables = Service::ProjectVariable.collection project_id: @rating.step.project_id
+  end
 
-	def sync
-		@rating = IloRating.find(params[:id])
-		if @rating.sync_numbers_with(variable_id: protected_sync_params[:variable_id])
-			redirect_to edit_step_path(@rating.step.name), notice: "Rating's numbers has been synchronized"
-		else
-			redirect_to show_sync_ilo_rating_path(@rating), alert: 'Please select variable to sync'
-		end
-	end
+  def sync
+    @rating = IloRating.find(params[:id])
+    if protected_sync_params[:variable_id].present?
+      @rating.sync_numbers_with(variable_id: protected_sync_params[:variable_id])
+      redirect_to edit_step_path(@rating.step.name), notice: "Rating's numbers has been synchronized"
+    else
+      redirect_to show_sync_ilo_rating_path(@rating), alert: 'Please select variable to sync'
+    end
+  end
 
-	private
+  private
 
-	def protected_advance_params
-		params.require(:ilo_rating).permit(:step_id, :client_from_date, :client_to_date, :code, :description)
-	end
+  def protected_advance_params
+    params.require(:ilo_rating).permit(:step_id, :client_from_date, :client_to_date, :code, :description)
+  end
 
-	def protected_basic_params
-		params.require(:ilo_rating).permit(:code, :description)
-	end
+  def protected_basic_params
+    params.require(:ilo_rating).permit(:code, :description)
+  end
 
-	def protected_sync_params
-		params.require(:ilo_rating).permit(:variable_id)
-	end
+  def protected_sync_params
+    params.require(:ilo_rating).permit(:variable_id)
+  end
 
 end
--- app/models/ilo_rating.rb
+++ app/models/ilo_rating.rb
@@ -1,127 +1,127 @@
 class IloRating < ActiveRecord::Base
-	validates :code, :step_id, presence: true
-	serialize :numbers, Array
+  validates :code, :step_id, presence: true
+  serialize :numbers, Array
 
-	belongs_to :step
+  belongs_to :step
 
-	validates :code, uniqueness: true
-	validates :code, length: { maximum: 2 }
+  validates :code, uniqueness: true
+  validates :code, length: { maximum: 2 }
 
-	attr_accessor :client_from_date, :client_to_date, :addresses
+  attr_accessor :client_from_date, :client_to_date, :addresses
 
-	validates :client_from_date, :"validator/date" => { date_format: Date::DEFAULT_FORMAT, field: :from_date }, if: Proc.new { |record| record.new_record? }
-	validates :client_to_date, :"validator/date" => { date_format: Date::DEFAULT_FORMAT, field: :to_date }, if: Proc.new { |record| record.new_record? }
+  validates :client_from_date, :"validator/date" => { date_format: Date::DEFAULT_FORMAT, field: :from_date }, if: Proc.new { |record| record.new_record? }
+  validates :client_to_date, :"validator/date" => { date_format: Date::DEFAULT_FORMAT, field: :to_date }, if: Proc.new { |record| record.new_record? }
 
-	validate :unique_date_range, on: :create
+  validate :unique_date_range, on: :create
 
-	SEPERATOR = ","
-	SEPERATOR_DISPLAY = ", "
+  SEPERATOR = ","
+  SEPERATOR_DISPLAY = ", "
 
-	NON_EXISTING = 0
-	EXISTING = 99
+  NON_EXISTING = 0
+  EXISTING = 999
 
-	class << self
-		def get_code date:, tel:
-			code = NON_EXISTING
+  class << self
+    def get_code date:, tel:
+      code = NON_EXISTING
 
-			all.each do |r|
-				if r.has_date? date
-					code = r.code
+      all.each do |r|
+        if r.has_date? date
+          code = r.code
 
-					if r.has_number? tel.without_prefix
-						code = EXISTING
-					end
+          if r.has_number? tel.without_prefix
+            code = EXISTING
+          end
 
-					break
-				end
-			end
+          break
+        end
+      end
 
-			code
-		end
+      code
+    end
 
-		def get date:, tel:
-			rating = nil
+    def get date:, tel:
+      rating = nil
 
-			all.each do |r|
-				rating = r if r.exist? date: date, tel: tel
-				break
-			end
+      all.each do |r|
+        rating = r if r.exist? date: date, tel: tel
+        break
+      end
 
-			rating
-		end
-	end
+      rating
+    end
+  end
 
-	def unique_date_range
-		if client_from_date.present? && client_to_date.present?
-			self.from_date = Parser::DateParser.parse(client_from_date)
-			self.to_date = Parser::DateParser.parse(client_to_date)
-			IloRating.all.each do |rating|
-				if rating.from_date.between?(from_date, to_date) || rating.to_date.between?(from_date, to_date)
-					errors.add(:date, 'date range already exists')
-				end
-			end
-		end
-	end
+  def unique_date_range
+    if client_from_date.present? && client_to_date.present?
+      self.from_date = Parser::DateParser.parse(client_from_date)
+      self.to_date = Parser::DateParser.parse(client_to_date)
+      IloRating.all.each do |rating|
+        if rating.from_date.between?(from_date, to_date) || rating.to_date.between?(from_date, to_date)
+          errors.add(:date, 'date range already exists')
+        end
+      end
+    end
+  end
 
-	def exist? tel:, date:
-		found = false
+  def exist? tel:, date:
+    found = false
 
-		if has_date?(date)
-			if has_number?(tel.without_prefix)
-				found = true
-			end
-		end
+    if has_date?(date)
+      if has_number?(tel.without_prefix)
+        found = true
+      end
+    end
 
-		found
-	end
+    found
+  end
 
-	def has_date? date
-		date.between?(from_date, to_date)
-	end
+  def has_date? date
+    date.between?(from_date, to_date)
+  end
 
-	def has_number? number
-		numbers.include?(number)
-	end
+  def has_number? number
+    numbers.include?(number)
+  end
 
-	def addresses
-		@addresses
-	end
+  def addresses
+    @addresses
+  end
 
-	def addresses=(val)
-		@addresses = val
-	end
+  def addresses=(val)
+    @addresses = val
+  end
 
-	def client_from_date
-		@client_from_date
-	end
+  def client_from_date
+    @client_from_date
+  end
 
-	def client_from_date=(val)
-		@client_from_date = val
-	end
+  def client_from_date=(val)
+    @client_from_date = val
+  end
 
-	def client_to_date
-		@client_to_date
-	end
+  def client_to_date
+    @client_to_date
+  end
 
-	def client_to_date=(val)
-		@client_to_date = val
-	end
+  def client_to_date=(val)
+    @client_to_date = val
+  end
 
-	def sync_numbers_with variable_id:
-		is_modified = false
+  def sync_numbers_with variable_id:
+    is_modified = false
 
-		call_log_answers = Service::CallLogAnswer.fetch_by project_id: step.project_id, variable_id: variable_id, from: from_date.to_string('%Y-%m-%d'), to: to_date.to_string('%Y-%m-%d')
-		call_log_answers.each do |answer|
-			if answer.value
-				is_modified = true
-				number_without_voip = answer.call_log[:prefix_called_number] ? answer.call_log[:address][answer.call_log[:prefix_called_number].length..-1] : answer.call_log[:address]
-				tel = Tel.new number_without_voip
-				numbers.push(tel.without_prefix) unless has_number?(tel.without_prefix)
-			end
-		end
+    call_log_answers = Service::CallLogAnswer.fetch_by project_id: step.project_id, variable_id: variable_id, from: from_date.to_string('%Y-%m-%d'), to: to_date.to_string('%Y-%m-%d')
+    call_log_answers.each do |answer|
+      if answer.value
+        is_modified = true
+        number_without_voip = answer.call_log[:prefix_called_number] ? answer.call_log[:address][answer.call_log[:prefix_called_number].length..-1] : answer.call_log[:address]
+        tel = Tel.new number_without_voip
+        numbers.push(tel.without_prefix) unless has_number?(tel.without_prefix)
+      end
+    end
 
-		save if is_modified
+    save if is_modified
 
-	end
+  end
 
 end
