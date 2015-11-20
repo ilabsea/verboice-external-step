a88b9e30c5cdca89664ffa96016693b6d009e290
kakada <kakada.chheang@gmail.com>
2015-11-20 11:19:24 +0700

    add required attributes to manifest

--- app/models/steps/date.rb
+++ app/models/steps/date.rb
@@ -1,7 +1,8 @@
 module Steps
   class Date
     def self.response options={}
-      date = DateUnit.new(options[:unit].downcase, options[:value].to_i).days.days.ago.strftime("%d/%m/%Y")
+      number_of_days = DateUnit.new(options[:unit].downcase, options[:value].to_i).days
+      date = number_of_days.days.ago.strftime("%d/%m/%Y") if number_of_days
       "{\"result\": \"#{date}\"}"
     end
   end
